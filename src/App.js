cbee143e134a11c6ab5e2a5a35b77f04f7d92806
Md. Nasim Ahmed <mdnasimahmed341@gmail.com>
2023-02-02 06:33:58 -0500

    implement react router dom

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 // import logo from './logo.svg';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import LoadCountries from './components/LoadCountries';
+import Home from './components/Home';
+import About from './components/About';
+import Products from './components/Products';
+import Notfound from './components/Notfound';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
-      <LoadCountries></LoadCountries>
+      <Header></Header>
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>\
+        <Route path='/about' element={<About></About>}></Route>
+        <Route path='/produtcs' element={<Products></Products>}></Route>
+        <Route path='/countries'  element={<LoadCountries></LoadCountries>}></Route>
+        <Route path='*' element={<Notfound></Notfound>}></Route>
+      </Routes>
+      <Footer></Footer>
     </div>
   );
 }
